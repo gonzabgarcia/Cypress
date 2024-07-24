e4397cbbdd98aeba3b748c7127f8f499a642ef74
gonzabgarcia <gonzalo.b.g.97@gmail.com>
2024-07-24 01:59:46 -0300

    Configurar workflow de Cypress y actualizar .gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -3,15 +3,20 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      // Configura el plugin Mochawesome
       try {
         require('cypress-mochawesome-reporter/plugin')(on);
-        require('@shelex/cypress-allure-plugin/writer')(on);
       } catch (error) {
         console.error('Error setting up plugins:', error);
       }
+      // Retorna la configuración
       return config;
     },
     baseUrl: 'http://localhost:3000', // Ajusta esto a la URL de tu aplicación
+    specPattern: 'cypress/integration/**/*.spec.js', // Especifica el patrón para los archivos de pruebas
+    supportFile: 'cypress/support/index.js', // Archivo de soporte para comandos globales y configuraciones adicionales
+    viewportWidth: 1280, // Ajusta el tamaño del viewport si es necesario
+    viewportHeight: 720, // Ajusta el tamaño del viewport si es necesario
   },
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
@@ -23,10 +28,5 @@ module.exports = defineConfig({
     reportPageTitle: 'Cypress Test Results',
     embeddedScreenshots: true,
     inlineAssets: true,
-  },
-  env: {
-    allure: {
-      outputDir: 'cypress/reports/allure-results',
-    },
   },
 });
